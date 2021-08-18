edc11fe9e3ed59c36135b6e8b173c9a93b203b3f
Dan Lee <danlee1025@gmail.com>
2021-08-18 08:50:06 -0400

    - Add constructor for cibmtrUrl and ccn - Add auth header - Change HTTP requests to use HttpClient

--- src/main/java/org/mitre/hapifhir/MedMorphToCIBMTR.java
+++ src/main/java/org/mitre/hapifhir/MedMorphToCIBMTR.java
@@ -16,15 +16,26 @@ import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Quantity;
 import org.json.JSONObject;
 import org.json.JSONArray;
-import java.io.OutputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import java.util.stream.Collectors;
 
 public class MedMorphToCIBMTR {
-  private final String CIBMTR_URL = "http://pathways.mitre.org:4444/";
+  private String cibmtrUrl;
+  private String ccn;
 
-  public void convert(Bundle medmorphReport) {
+  public MedMorphToCIBMTR(String cibmtrUrl, String ccn) {
+    this.cibmtrUrl = cibmtrUrl;
+    this.ccn = ccn;
+  }
+
+  public void convert(Bundle medmorphReport, String authToken) {
     // https://fhir.nmdp.org/ig/cibmtr-reporting/CIBMTR_Direct_FHIR_API_Connection_Guide_STU3.pdf
     if (medmorphReport.hasEntry()) {
       List<BundleEntryComponent> entriesList = medmorphReport.getEntry();
@@ -32,24 +43,23 @@ public class MedMorphToCIBMTR {
       if (patientEntry == null) return;
 
       Patient patient = (Patient) patientEntry.getResource();
-      String crid = getCrid(patient);
-      String resourceId = postPatient(crid);
+      String crid = getCrid(authToken, patient);
+      String resourceId = postPatient(authToken, crid);
 
-      if (resourceId != null) postBundle(entriesList, resourceId);
+      if (resourceId != null) postBundle(authToken, entriesList, resourceId);
     }
   }
 
   // Register patient with CIBMTR and returns CRID
-  private String getCrid(Patient patient) {
-    try {
-      URL cridUrl = new URL(CIBMTR_URL + "CRID");
-      HttpURLConnection connection = (HttpURLConnection) cridUrl.openConnection();
-      connection.setRequestMethod("PUT");
-      connection.setDoOutput(true);
-      connection.setRequestProperty("Content-Type", "application/json");
+  private String getCrid(String authToken, Patient patient) {
+    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpPut httpPut = new HttpPut(cibmtrUrl + "CRID");
+      httpPut.setHeader("Accept", "application/json");
+      httpPut.setHeader("Content-type", "application/json");
+      httpPut.setHeader("Authorization", "Bearer " + authToken);
 
       JSONObject cridRequestBody = new JSONObject();
-      cridRequestBody.put("ccn", "12345");
+      cridRequestBody.put("ccn", ccn);
       JSONObject patientJson = new JSONObject();
       patientJson.put("firstName", patient.getName().get(0).getGiven().get(0));
       patientJson.put("lastName", patient.getName().get(0).getFamily());
@@ -57,17 +67,14 @@ public class MedMorphToCIBMTR {
       patientJson.put("gender", patient.getGender().getDisplay());
       cridRequestBody.put("patient", patientJson);
 
-      OutputStream os = connection.getOutputStream();
-      byte[] input = cridRequestBody.toString().getBytes("utf-8");
-      os.write(input, 0, input.length);
+      StringEntity stringEntity = new StringEntity(cridRequestBody.toString());
+      ResponseHandler<String> responseHandler = response -> {
+        HttpEntity entity = response.getEntity();
+        return entity != null ? EntityUtils.toString(entity) : null;
+      };
 
-      BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
-      StringBuilder response = new StringBuilder();
-      String responseLine = null;
-      while ((responseLine = br.readLine()) != null) {
-          response.append(responseLine.trim());
-      }
-      JSONObject responseObj = new JSONObject(response.toString());
+      String responseBody = httpClient.execute(httpPut, responseHandler);
+      JSONObject responseObj = new JSONObject(responseBody.toString());
       JSONArray perfectMatch = responseObj.getJSONArray("perfectMatch");
       if (!perfectMatch.isEmpty()) return (String)perfectMatch.getJSONObject(0).get("crid");
     } catch (Exception e) {
@@ -78,15 +85,13 @@ public class MedMorphToCIBMTR {
   }
 
   // POST Patient resource with CRID and return resource id
-  private String postPatient(String crid) {
+  private String postPatient(String authToken, String crid) {
     if (crid == null) return null;
 
-    try {
-      URL patientUrl = new URL(CIBMTR_URL + "Patient");
-      HttpURLConnection patientConnection = (HttpURLConnection) patientUrl.openConnection();
-      patientConnection.setRequestMethod("POST");
-      patientConnection.setDoOutput(true);
-      patientConnection.setRequestProperty("Content-Type", "application/fhir+json");
+    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpPost httpPost = new HttpPost(cibmtrUrl + "Patient");
+      httpPost.setHeader("Content-Type", "application/fhir+json");
+      httpPost.setHeader("Authorization", "Bearer " + authToken);
 
       JSONObject patientRequestBody = new JSONObject();
       patientRequestBody.put("resourceType", "Patient");
@@ -100,43 +105,39 @@ public class MedMorphToCIBMTR {
       identifierArray.put(identifierObject);
       patientRequestBody.put("identifier", identifierArray);
 
-      OutputStream os = patientConnection.getOutputStream();
-      byte[] input = patientRequestBody.toString().getBytes("utf-8");
-      os.write(input, 0, input.length);
+      StringEntity stringEntity = new StringEntity(patientRequestBody.toString());
+      ResponseHandler<String> responseHandler = response -> {
+        int status = response.getStatusLine().getStatusCode();
+        if (status == 200) {
+          String location = response.getFirstHeader("Location").getValue();
+          int index = location.indexOf("Patient/");
+          if (index > 0) return location.substring(index + 8);
+        }
 
-      int responseCode = patientConnection.getResponseCode();
-      if (responseCode == 200) {
-        // Location header response field contains a URL that includes the resource id after 'Patient/'
-        Map<String, List<String>> map = patientConnection.getHeaderFields();
-        String location = map.get("Location").get(0);
-        int index = location.indexOf("Patient/");
-        if (index > 0) return location.substring(index + 8);
-      }
+        return null;
+      };
+
+      return httpClient.execute(httpPost, responseHandler);
     } catch (Exception e) {
       return null;
     }
-    
-    return null;
   }
 
   // Post bundle of observations
-  private void postBundle(List<BundleEntryComponent> entries, String resourceId) {
+  private void postBundle(String authToken, List<BundleEntryComponent> entries, String resourceId) {
     List<BundleEntryComponent> observationEntries = entries.stream().filter(entry -> entry.getResource().getResourceType() == ResourceType.Observation).collect(Collectors.toList());
-    try {
-      URL bundleUrl = new URL(CIBMTR_URL + "Bundle");
-      HttpURLConnection bundleConnection = (HttpURLConnection) bundleUrl.openConnection();
-      bundleConnection.setRequestMethod("POST");
-      bundleConnection.setDoOutput(true);
-      bundleConnection.setRequestProperty("Content-Type", "application/fhir+json");
+    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpPost httpPost = new HttpPost(cibmtrUrl + "Bundle");
+      httpPost.setHeader("Content-Type", "application/fhir+json");
+      httpPost.setHeader("Authorization", "Bearer " + authToken);
 
       JSONObject bundleRequestBody = new JSONObject();
       bundleRequestBody.put("resourceType", "Bundle");
       bundleRequestBody.put("type", "transaction");
       bundleRequestBody.put("entry", getObservationEntries(observationEntries, resourceId));
 
-      OutputStream os = bundleConnection.getOutputStream();
-      byte[] input = bundleRequestBody.toString().getBytes("utf-8");
-      os.write(input, 0, input.length);
+      StringEntity stringEntity = new StringEntity(bundleRequestBody.toString());
+      httpClient.execute(httpPost);
     } catch (Exception e) {
       return;
     }
