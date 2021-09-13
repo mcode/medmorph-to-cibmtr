d5736cdf9ce0fa6522695e7933e0bc194a642528
Dan Lee <danlee1025@gmail.com>
2021-09-13 16:22:39 -0400

    Remove ccn from constructor and add skeleton for getCcn method.

--- src/main/java/org/mitre/hapifhir/MedMorphToCIBMTR.java
+++ src/main/java/org/mitre/hapifhir/MedMorphToCIBMTR.java
@@ -1,58 +1,58 @@
 package org.mitre.hapifhir;
 
-import java.util.List;
-import java.util.Map;
-import java.net.URL;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
-import org.hl7.fhir.r4.model.Resource;
-import org.hl7.fhir.r4.model.ResourceType;
-import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.Observation;
-import org.hl7.fhir.r4.model.CodeableConcept;
-import org.hl7.fhir.r4.model.Coding;
-import org.hl7.fhir.r4.model.Quantity;
-import org.json.JSONObject;
-import org.json.JSONArray;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import java.util.stream.Collectors;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.MessageHeader;
+import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Quantity;
+import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.ResourceType;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MedMorphToCIBMTR {
   private String cibmtrUrl;
-  private String ccn;
 
-  public MedMorphToCIBMTR(String cibmtrUrl, String ccn) {
+  public MedMorphToCIBMTR(String cibmtrUrl) {
     this.cibmtrUrl = cibmtrUrl;
     if (!this.cibmtrUrl.endsWith("/")) this.cibmtrUrl += "/";
-    this.ccn = ccn;
   }
 
-  public void convert(Bundle medmorphReport, String authToken) {
+  public void convert(Bundle medmorphReport, MessageHeader messageHeader, String authToken) {
     // https://fhir.nmdp.org/ig/cibmtr-reporting/CIBMTR_Direct_FHIR_API_Connection_Guide_STU3.pdf
     if (medmorphReport.hasEntry()) {
       List<BundleEntryComponent> entriesList = medmorphReport.getEntry();
       BundleEntryComponent patientEntry = entriesList.stream().filter(entry -> entry.getResource().getResourceType() == ResourceType.Patient).findAny().orElse(null);
-      if (patientEntry == null) return;
+      String ccn = getCcn(entriesList, messageHeader);
+      if (patientEntry == null || ccn == null) return;
 
       Patient patient = (Patient) patientEntry.getResource();
-      Number crid = getCrid(authToken, patient);
-      String resourceId = postPatient(authToken, crid.toString());
+      Number crid = getCrid(authToken, ccn, patient);
+      String resourceId = postPatient(authToken, ccn, crid.toString());
 
-      if (resourceId != null) postBundle(authToken, entriesList, resourceId);
+      if (resourceId != null) postBundle(authToken, ccn, entriesList, resourceId);
     }
   }
 
   // Register patient with CIBMTR and returns CRID
-  protected Number getCrid(String authToken, Patient patient) {
+  protected Number getCrid(String authToken, String ccn, Patient patient) {
     try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
       HttpPut httpPut = new HttpPut(cibmtrUrl + "CRID");
       httpPut.setHeader("Accept", "application/json");
@@ -87,7 +87,7 @@ public class MedMorphToCIBMTR {
   }
 
   // POST Patient resource with CRID and return resource id
-  protected String postPatient(String authToken, String crid) {
+  protected String postPatient(String authToken, String ccn, String crid) {
     if (crid == null) return null;
 
     try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
@@ -97,7 +97,7 @@ public class MedMorphToCIBMTR {
 
       JSONObject patientRequestBody = new JSONObject();
       patientRequestBody.put("resourceType", "Patient");
-      patientRequestBody.put("meta", getMeta());
+      patientRequestBody.put("meta", getMeta(ccn));
       patientRequestBody.put("text", (new JSONObject()).put("status", "empty"));
       JSONArray identifierArray = new JSONArray();
       JSONObject identifierObject = new JSONObject();
@@ -127,7 +127,7 @@ public class MedMorphToCIBMTR {
   }
 
   // Post bundle of observations
-  protected void postBundle(String authToken, List<BundleEntryComponent> entries, String resourceId) {
+  protected void postBundle(String authToken, String ccn, List<BundleEntryComponent> entries, String resourceId) {
     List<BundleEntryComponent> observationEntries = entries.stream().filter(entry -> entry.getResource().getResourceType() == ResourceType.Observation).collect(Collectors.toList());
     try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
       HttpPost httpPost = new HttpPost(cibmtrUrl + "Bundle");
@@ -137,7 +137,7 @@ public class MedMorphToCIBMTR {
       JSONObject bundleRequestBody = new JSONObject();
       bundleRequestBody.put("resourceType", "Bundle");
       bundleRequestBody.put("type", "transaction");
-      bundleRequestBody.put("entry", getObservationEntries(observationEntries, resourceId));
+      bundleRequestBody.put("entry", getObservationEntries(ccn, observationEntries, resourceId));
 
       StringEntity stringEntity = new StringEntity(bundleRequestBody.toString());
       httpPost.setEntity(stringEntity);
@@ -147,7 +147,7 @@ public class MedMorphToCIBMTR {
     }
   }
 
-  protected JSONArray getObservationEntries(List<BundleEntryComponent> observationEntries, String resourceId) {
+  protected JSONArray getObservationEntries(String ccn, List<BundleEntryComponent> observationEntries, String resourceId) {
     JSONArray entryArray = new JSONArray();
 
     for (BundleEntryComponent entry : observationEntries) {
@@ -160,7 +160,7 @@ public class MedMorphToCIBMTR {
       Observation observation = (Observation)entry.getResource();
       JSONObject observationResourceObject = new JSONObject();
       observationResourceObject.put("resourceType", "Observation");
-      observationResourceObject.put("meta", getMeta());
+      observationResourceObject.put("meta", getMeta(ccn));
       observationResourceObject.put("subject", (new JSONObject()).put("reference", "Patient/" + resourceId));
       observationResourceObject.put("effectiveDateTime", observation.getEffectiveDateTimeType().dateTimeValue().getValue());
       CodeableConcept code = observation.getCode();
@@ -186,7 +186,7 @@ public class MedMorphToCIBMTR {
     return entryArray;
   }
 
-  protected JSONObject getMeta() {
+  protected JSONObject getMeta(String ccn) {
     JSONObject metaObject = new JSONObject();
     JSONArray securityArray = new JSONArray();
     JSONObject securityObject = new JSONObject();
@@ -196,5 +196,10 @@ public class MedMorphToCIBMTR {
     metaObject.put("security", securityArray);
 
     return metaObject;
+  }
+
+  // Extracts CCN from MessageHeader.sender.identifier
+  protected String getCcn(List<BundleEntryComponent> bundleEntries, MessageHeader messageHeader) {
+    return null;
   }
 }
